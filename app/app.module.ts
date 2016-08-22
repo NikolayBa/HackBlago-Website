36ae1a791d44660f5129a8d798e5e7a1428a2762
Nikola <nekavsitam@gmail.com>
2016-08-22 12:18:43 +0300

    Started ApiService

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule }      from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
