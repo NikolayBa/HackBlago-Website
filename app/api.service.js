8513a1994f57c1163726e3b30bd5b89242f801ca
Nikola <nekavsitam@gmail.com>
2016-09-01 08:52:51 +0300

    Fixed file paths

--- app/api.service.js
+++ app/api.service.js
@@ -11,12 +11,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var http_1 = require('@angular/http');
 var api_request_1 = require('./api-request');
+/**
+ * We declare the ApiService as Injectable in order to have the
+ * dependency injector provide it to all classes that might need it.
+ * set and get methods are used in order to access data. The set/get functions begin with a $
+*/
 var ApiService = (function () {
     function ApiService(http) {
         this.http = http;
         this.$apiRequest = new api_request_1.ApiRequest();
     }
     ApiService.prototype.registerUser = function (name, password_hash, email, github) {
+        this.refreshParameters();
         this.$api_key = ApiService.api_key;
         this.$functionName = ApiService.register_user_function;
         this.$name = name;
@@ -27,9 +33,27 @@ var ApiService = (function () {
         console.log(JSON.stringify(this.apiRequest));
     };
     ApiService.prototype.authenticateUser = function (name, password_hash) {
+        this.refreshParameters();
         this.$functionName = ApiService.authenticate_user_function;
         this.$name = name;
         this.$password_hash = password_hash;
+    };
+    ApiService.prototype.resetUserPassword = function (name) {
+        this.refreshParameters();
+        this.$name = name;
+    };
+    ApiService.prototype.inviteTeamMemberByUsername = function (team_name, user_name) {
+        this.refreshParameters();
+    };
+    ApiService.prototype.refreshParameters = function () {
+        this.$apiRequest.$payload.$parameters = new Object();
+    };
+    /**
+     * @Nullable github
+     */
+    ApiService.prototype.registerTeam = function (name, github) {
+        this.$name = name;
+        this.$github = github;
     };
     Object.defineProperty(ApiService.prototype, "$functionName", {
         set: function (value) {
@@ -83,6 +107,7 @@ var ApiService = (function () {
         enumerable: true,
         configurable: true
     });
+    //define static constants for easier usage
     ApiService.api_key = "test_key";
     ApiService.register_user_function = "register_user";
     ApiService.authenticate_user_function = "authenticate_user";
